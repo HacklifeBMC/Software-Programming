f561f45e2f23d08a2ec6466de677931b7d0feaa9
christian basubi <basubichristian@gmail.com>
2017-01-31 00:37:04 -0500

    Add files via upload

---
+++ Testpg.c
@@ -0,0 +1,24 @@
+//
+//  Testpg.c
+//  
+//
+//  Created by BASUBI M Christian on 30/01/2017.
+//
+//
+
+#include "Testpg.h"
+
+int main ()
+{
+    int c,n,fact = 1;
+    
+    printf("enter a number to calculate the factorial \n");
+    scanf("%d", &n);
+    
+    for (c= 1; c<=n; c++)
+        fact = fact * c;
+    printf("Factorial of %d =  %d\n",n,fact);
+    
+    return 0;
+    
+}
